15ba0bed3de5d5f07949a974c3bca538d6f0ca45
Patrick Killean <pat.killean@gmail.com>
2018-07-12 18:36:02 -0400

    make no-include configurable

--- src/wire_report/error.cljs
+++ src/wire_report/error.cljs
@@ -28,30 +28,69 @@
      :file (subs js-address 0 (.-index (.exec #":\d" js-address)))
      :var var}))
 
-(defn user-stack
-  "starting at top of error stack, assemble chain down to first call that
-   is not a clojure/goog namespace" ;- make extensible
-  [stack]
-  (let [acc #js[]]
-    (loop [stack (rest stack)]
-      (when-let [line (first stack)]
-        (when (not (string/includes? line "cljs/test.js"))
-          (let [{v :var :as var-info} (stackline->var-info line)]
-            (.push acc var-info)
-            (recur (rest stack))))))
-    (vec acc)))
+(defn includes-any?
+  [st bad-strings]
+  (loop [bad-strings bad-strings]
+    (if-let [bad (first bad-strings)]
+      (or (string/includes? st bad)
+          (recur (rest bad-strings)))
+      false)))
 
-(defn err->map [err]
+(defn user-stack
+  "starting at top of error stack, collect all avail info from each stackline
+   until it hits no-include string. By default it keeps everything up to any
+   \"cljs/test.js\". Pass a coll of strings to narrow accepted stack lines.
+   Each test is conducted on the raw stack line"
+  ([stack] (user-stack stack nil))
+  ([stack no-include]
+   (when no-include
+     (assert (and (seq no-include) (every? string? no-include))))
+   (let [stack (if (vector? stack) stack (string/split-lines stack))
+         no-include-strings (into ["cljs/test.js"] no-include)
+         acc #js[]]
+     (loop [stack (rest stack)]
+       (when-let [line (first stack)]
+         (let []
+           (when (not (includes-any? line no-include-strings))
+             (let [{v :var :as var-info} (stackline->var-info line)]
+               (.push acc var-info)
+               (recur (rest stack)))))))
+     (vec acc))))
+
+(defn err->map
+  "convert a js-error into a more ergonomic map
+   opts:
+     :no-include coll<string>
+       - strings used to restrict collected stack lines
+
+   The point of the user-stack is to remove noise in a raw error stack by providing
+   a cutoff point via a no-include string test. For example, in a test namespace,
+   when a test has an uncaught error, you can safely assume that the error occured
+   in YOUR code, not the cljs.test harness. So all stacklines that are part of cljs.test
+   are automatically removed. From here the 'incident' is the deepest point in
+   the user-stack that passes the no-include test. This may or not be what you want
+   but its a start.
+
+   ret:
+    {:message 'same as js-error'
+     :type :TypeError
+     :var symbol-from-incident-line
+       - may not exist
+       - cljs.core/demunge broken, see https://dev.clojure.org/jira/browse/CLJS-1726
+         - maybe use devtools.munging/break-into-parts, use as symbols to lookup in cache?
+     :js-address {:file 'pathto/file.js'
+                  :col number
+                  :row number
+                  :stack-line 'raw-stack-line'}
+     :user-stack [{:var :col :row :file} {..} {..} ...]
+     :raw js/Error}"
+  [err &{:keys [no-include] :no-include nil}]
   (assert (instance? js/Error err))
-  (let [msg (pr-str (.-message err))
-        stack (string/split-lines (.-stack err))
-        user-stack (user-stack stack)
+  (let [user-stack (user-stack (.-stack err) no-include)
         incident (last user-stack)]
-    {:message msg
+    {:message (.-message err)
      :type (keyword (.-name err))
      :var (get incident :var)
      :js-address (dissoc incident :var)
-     ; :raw-error err
-     ; :stack stack
      :user-stack user-stack
      :raw err}))
--- src/wire_report/error_test.cljs
+++ src/wire_report/error_test.cljs
@@ -4,13 +4,34 @@
             [wire-report.error :as E]))
 
 (deftest err->map-test
-  (testing "calling undefined function"
+  (testing "first stack line"
     (try
       (bar)
       (catch js/Error e
         (let [m (E/err->map e)]
-          (is (= (:var m) 'wire-report.error-test.err->map-test))
           (is (= (:type m) :TypeError))
-          (is (string/includes? (:message m) "Cannot read property 'call' of undefined"))
-          (is (string/ends-with? (get-in m [:js-address :file]) "wire_report/error_test.js")))))))
+          ; (is (= (:var m) 'wire-report.error-test/err->map-test))
+          (is (= 1 (count (get m :user-stack))))
+          (is (= (:message m) "Cannot read property 'call' of undefined"))
+          (is (string/ends-with? (get-in m [:js-address :file]) "wire_report/error_test.js"))))))
+  (testing "indirect"
+    (try
+      (string/ends-with? nil "55")
+      (catch js/Error e
+        (let [m (E/err->map e)]
+          (is (= (:type m) :TypeError))
+          (is (= 3 (count (get m :user-stack))))
+          ; (is (= (:var m) 'wire-report.error-test/err->map-test))
+          (is (= (:message m) "Cannot read property 'length' of null"))
+          (is (string/ends-with? (get-in m [:js-address :file]) "wire_report/error_test.js"))))))
+  (testing "same but filter this namespace"
+    (try
+      (string/ends-with? nil "55")
+      (catch js/Error e
+        (let [m (E/err->map e :no-include ["wire_report/error_test.js"])]
+          (is (= (:type m) :TypeError))
+          (is (= 2 (count (get m :user-stack))))
+          ; (is (= (:var m) 'clojure.string/ends-with?))
+          (is (= (:message m) "Cannot read property 'length' of null"))
+          (is (string/ends-with? (get-in m [:js-address :file]) "string.js")))))))
 

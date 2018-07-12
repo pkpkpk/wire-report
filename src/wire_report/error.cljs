bc61a1ede2b375e798b84ecca7153e12cf7a547f
Patrick Killean <pat.killean@gmail.com>
2018-07-12 13:37:10 -0400

    user-stack

--- src/wire_report/error.cljs
+++ src/wire_report/error.cljs
@@ -5,6 +5,7 @@
 
 (defn stackline->var-info ; v8 specific?
   [line]
+  (assert (string? line))
   (let [line (string/replace-first line  #"^ +at " "")
         [munged js-address] (if (mapped-line? line)
                               (string/split line #" \(")
@@ -23,36 +24,61 @@
                       (rest (array-seq (.split (subs js-address i) ":"))))]
     {:col col
      :row row
-     :raw js-address ;<= devtools treats as url
+     :stack-line line ;<= devtools treats as url
      :file (subs js-address 0 (.-index (.exec #":\d" js-address)))
      :var var}))
 
-(defn classify-msg [msg] ;=> ?map
-  (cond
-    (string/includes? msg "property 'call'")
-    {:type :bad-function-call}
+(defn classify-err [err] ;=> ?map
+  (assert (instance? js/Error err))
+  (let [msg (.-message err)
+        error-type (.-name err)]
+    ;;consider TypeError etc
+    (cond
+      (string/includes? msg "property 'call'")
+      :bad-function-call
 
-    (string/includes? msg "Invalid arity:")
-    {:type :invalid-arity}
+      (string/includes? msg "Invalid arity:")
+      :invalid-arity
 
-    (string/includes? msg "is not a function")
-    {:type :not-a-function}
+      (string/includes? msg "is not a function")
+      :not-a-function
 
-    (and (string/includes? msg "Cannot read property")
-         (or (string/includes? msg "of undefined")
-             (string/includes? msg "of null")))
-    {:type :undefined-type
-     :suggestion :typo?}
+      (and (string/includes? msg "Cannot read property")
+           (= "TypeError" (.-name err)))
+      :TypeError
 
-    :else {}))
+      (and (string/includes? msg "Cannot read property")
+           (or (string/includes? msg "of undefined")
+               (string/includes? msg "of null")))
+      :undefined-type
+
+      :else :unclassified)))
+
+(defn user-stack
+  "starting at top of error stack, assemble chain down to first call that
+   is not a clojure/goog namespace" ;- make extensible
+  [stack]
+  (let [acc #js[]]
+    (loop [stack (rest stack)]
+      (when-let [line (first stack)]
+        (when (not (string/includes? line "cljs/test.js"))
+          (let [{v :var :as var-info} (stackline->var-info line)]
+            (.push acc var-info)
+            (recur (rest stack))))))
+    (vec acc)))
 
 (defn err->map [err]
   (let [msg (pr-str (.-message err))
         stack (string/split-lines (.-stack err))
-        var-info (stackline->var-info (second stack))
+        user-stack (user-stack stack)
+        incident (last user-stack)
+        etype (classify-err err)
         common {:message msg
-                :var (get var-info :var)
-                :js-address (dissoc var-info :var)
-                :raw-error err
-                :stack stack}]
-    (merge common (classify-msg msg))))
+                :type etype
+                :var (get incident :var)
+                :js-address (dissoc incident :var)
+                ; :raw-error err
+                ; :stack stack
+                :user-stack user-stack
+                :raw err}]
+    common))

39eb0227ee66a9934f8ab45e93f11bb5b7e36277
Patrick Killean <pat.killean@gmail.com>
2018-07-12 20:43:32 -0400

    use cljs.stacktrace

--- src/wire_report/error.cljs
+++ src/wire_report/error.cljs
@@ -1,32 +1,6 @@
 (ns wire-report.error
-  (:require [clojure.string :as string]))
-
-(defn mapped-line? [line] (.test #"\d\)$" line))
-
-(defn stackline->var-info ; v8 specific?
-  [line]
-  (assert (string? line))
-  (let [line (string/replace-first line  #"^ +at " "")
-        [munged js-address] (if (mapped-line? line)
-                              (string/split line #" \(")
-                              [nil line])
-        var (when munged
-              (->  munged
-                   (string/replace-first  #"\.cljs\$lang\$test" "")
-                   string/trim
-                   demunge
-                   symbol))
-        [row col] (into []
-                      (comp
-                       (remove empty?)
-                       (map #(string/replace % #"\)" ""))
-                       (map #(js/parseInt %)))
-                      (rest (array-seq (.split (subs js-address i) ":"))))]
-    {:col col
-     :row row
-     :stack-line line ;<= devtools treats as url
-     :file (subs js-address 0 (.-index (.exec #":\d" js-address)))
-     :var var}))
+  (:require [clojure.string :as string]
+            [cljs.stacktrace :as stacktrace]))
 
 (defn includes-any?
   [st bad-strings]
@@ -36,61 +10,60 @@
           (recur (rest bad-strings)))
       false)))
 
-(defn user-stack
-  "starting at top of error stack, collect all avail info from each stackline
-   until it hits no-include string. By default it keeps everything up to any
-   \"cljs/test.js\". Pass a coll of strings to narrow accepted stack lines.
-   Each test is conducted on the raw stack line"
-  ([stack] (user-stack stack nil))
-  ([stack no-include]
-   (when no-include
-     (assert (and (seq no-include) (every? string? no-include))))
-   (let [stack (if (vector? stack) stack (string/split-lines stack))
-         no-include-strings (into ["cljs/test.js"] no-include)
-         acc #js[]]
-     (loop [stack (rest stack)]
-       (when-let [line (first stack)]
-         (let []
-           (when (not (includes-any? line no-include-strings))
-             (let [{v :var :as var-info} (stackline->var-info line)]
-               (.push acc var-info)
-               (recur (rest stack)))))))
-     (vec acc))))
+(defn _demunge [munged]
+  (when munged
+    (->  munged
+         (string/replace-first  #"\.cljs\$lang\$test" "")
+         string/trim
+         demunge
+         symbol)))
+
+(def ^:dynamic *ua-product* :chrome)
 
 (defn err->map
   "convert a js-error into a more ergonomic map
    opts:
-     :no-include coll<string>
+     :exclude-files coll<string>
        - strings used to restrict collected stack lines
 
    The point of the user-stack is to remove noise in a raw error stack by providing
-   a cutoff point via a no-include string test. For example, in a test namespace,
-   when a test has an uncaught error, you can safely assume that the error occured
-   in YOUR code, not the cljs.test harness. So all stacklines that are part of cljs.test
-   are automatically removed. From here the 'incident' is the deepest point in
-   the user-stack that passes the no-include test. This may or not be what you want
-   but its a start.
+   a cutoff point via a string/includes? test. For example, when a test has an
+   uncaught error, you can safely assume that the error occured in YOUR code,
+   not the cljs.test harness. So all stacklines that are cljs.test and deeper
+   are automatically removed (cljs/test.js is built-in).
 
-   ret:
+   From here the :incident is the deepest point in the user-stack that passes
+   the file-exclusions. This may or may not be what you want but its a start.
+
     {:message 'same as js-error'
      :type :TypeError
-     :var symbol-from-incident-line
-       - may not exist
-       - cljs.core/demunge broken, see https://dev.clojure.org/jira/browse/CLJS-1726
-         - maybe use devtools.munging/break-into-parts, use as symbols to lookup in cache?
-     :js-address {:file 'pathto/file.js'
-                  :col number
-                  :row number
-                  :stack-line 'raw-stack-line'}
-     :user-stack [{:var :col :row :file} {..} {..} ...]
+     :incident -> deepest user-stack map,
+        {:file 'pathto/file.js'
+         :column number
+         :line number
+         :function 'string$from$raw_trace'
+         :var 'demunged.function/sy
+            - may not exist & my not be correct if it does
+            - cljs.core/demunge broken, see https://dev.clojure.org/jira/browse/CLJS-1726
+              - maybe use devtools.munging/break-into-parts, use as symbols to lookup in cache?}
+     :user-stack [{:function :column :line :file :var} {..} {..} ...]
+       - includes :incident in last spot
      :raw js/Error}"
-  [err &{:keys [no-include] :no-include nil}]
+  [err &{:keys [exclude-files] :exclude-files nil}]
   (assert (instance? js/Error err))
-  (let [user-stack (user-stack (.-stack err) no-include)
-        incident (last user-stack)]
+  (let [stack (stacktrace/parse-stacktrace {} (.-stack err) {:ua-product *ua-product*} {:asset-root ""})
+        exclude-files (into ["cljs/test.js"] exclude-files)
+        user-stack (into []
+                         (comp
+                          (take-while
+                           (fn [{file :file :as m}]
+                             (not (includes-any? file exclude-files))))
+                          (map
+                           (fn [{f :function :as m}]
+                             (assoc m :var (_demunge f)))))
+                         (rest stack))]
     {:message (.-message err)
      :type (keyword (.-name err))
-     :var (get incident :var)
-     :js-address (dissoc incident :var)
+     :incident (last user-stack)
      :user-stack user-stack
      :raw err}))
--- src/wire_report/error_test.cljs
+++ src/wire_report/error_test.cljs
@@ -1,7 +1,8 @@
 (ns wire-report.error-test
   (:require [cljs.test :refer-macros [deftest is testing]]
             [clojure.string :as string]
-            [wire-report.error :as E]))
+            [wire-report.error :as E]
+            [cljs.stacktrace :as stacktrace]))
 
 (deftest err->map-test
   (testing "first stack line"
@@ -9,29 +10,29 @@
       (bar)
       (catch js/Error e
         (let [m (E/err->map e)]
+        ; (is (= (:var m) 'wire-report.error-test/err->map-test))
           (is (= (:type m) :TypeError))
-          ; (is (= (:var m) 'wire-report.error-test/err->map-test))
           (is (= 1 (count (get m :user-stack))))
           (is (= (:message m) "Cannot read property 'call' of undefined"))
-          (is (string/ends-with? (get-in m [:js-address :file]) "wire_report/error_test.js"))))))
+          (is (string/ends-with? (get-in m [:incident :file]) "wire_report/error_test.js"))))))
   (testing "indirect"
     (try
       (string/ends-with? nil "55")
       (catch js/Error e
         (let [m (E/err->map e)]
+        ;; (is (= (:var m) 'wire-report.error-test/err->map-test))
           (is (= (:type m) :TypeError))
           (is (= 3 (count (get m :user-stack))))
-          ; (is (= (:var m) 'wire-report.error-test/err->map-test))
           (is (= (:message m) "Cannot read property 'length' of null"))
-          (is (string/ends-with? (get-in m [:js-address :file]) "wire_report/error_test.js"))))))
+          (is (string/ends-with? (get-in m [:incident :file]) "wire_report/error_test.js"))))))
   (testing "same but filter this namespace"
     (try
       (string/ends-with? nil "55")
       (catch js/Error e
-        (let [m (E/err->map e :no-include ["wire_report/error_test.js"])]
+        (let [m (E/err->map e :exclude-files ["wire_report/error_test.js"])]
+        ; (is (= (:var m) 'clojure.string/ends-with?))
           (is (= (:type m) :TypeError))
           (is (= 2 (count (get m :user-stack))))
-          ; (is (= (:var m) 'clojure.string/ends-with?))
           (is (= (:message m) "Cannot read property 'length' of null"))
-          (is (string/ends-with? (get-in m [:js-address :file]) "string.js")))))))
+          (is (string/ends-with? (get-in m [:incident :file]) "string.js")))))))
 

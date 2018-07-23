194a052de9019990013ae94d730afa73024ecee4
Patrick Killean <pat.killean@gmail.com>
2018-07-23 06:52:20 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,31 +1,12 @@
-(defproject wire-report "0.1.0"
+(defproject wire-report "0.2.0"
   :description "Send clojurescript node tests results over the wire"
   :url "https://github.com/pkpkpk/wire-report"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [com.cognitect/transit-cljs "0.8.239"]
-                 [com.cognitect/transit-clj "0.8.288"]
-                 [org.clojure/core.async "0.2.395"
-                  :exclusions [org.clojure/tools.reader]]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [com.cognitect/transit-cljs "0.8.256"]
+                 [org.clojure/core.async "0.4.474"]]
 
-  :plugins [[lein-figwheel "0.5.4-7"]
-            [lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]]
-
-  :source-paths ["src"]
-
-  :clean-targets ^{:protect false} ["target"]
-
-  :cljsbuild {:builds
-              [{:id "dev"
-                :source-paths ["src"]
-                :compiler {:main wire-report.runner
-                           :target :nodejs
-                           :asset-path "target/out"
-                           :output-to "target/wire_report.js"
-                           :output-dir "target/out"
-                           :source-map-timestamp true
-                           :parallel-build true
-                           :cache-analysis true}}]})
+  :source-paths ["src/main"])
